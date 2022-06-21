f84caf674861651a6bf4d5e63c859d5c2abc67fb
AtulSikarwal <atulsikarwal5.com>
2022-06-21 16:43:18 +0530

    The code for Find Max String Value

---
+++ FindMaxInt/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericsAss
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Method method = new Method();
+            Console.WriteLine(method.intMax(50, 39, 22));
+        
+            
+        }
+    }
+}
