dcd62a24cf1dee3efe14d234eb0d152ba4032d18
AtulSikarwal <atulsikarwal5.com>
2022-06-21 16:26:24 +0530

    The code for Find Max Value of Float Integer

---
+++ FindMaxFloat/Method.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAss
+{
+    internal class Method
+    {
+        public int intMax(int FirstNum,int SecondNum,int ThirdNum)
+        {
+            if (FirstNum.CompareTo(SecondNum) > 0 && FirstNum.CompareTo(ThirdNum) > 0)
+            {
+                return FirstNum;
+            }
+            if (SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) > 0)
+            {
+                return SecondNum;
+            }
+            if (ThirdNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(FirstNum) > 0)
+            {
+                return ThirdNum;
+            }
+            throw new Exception("firstnumber,secondnumber and thirdnumber are same");
+        }
+
+
+
+        public double floatMax(double FirstNum, double SecondNum, double ThirdNum)
+        {
+            if (FirstNum.CompareTo(SecondNum) > 0 && FirstNum.CompareTo(ThirdNum) > 0)
+            {
+                return FirstNum;
+            }
+            if (SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) > 0)
+            {
+                return SecondNum;
+            }
+            if (ThirdNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(FirstNum) > 0)
+            {
+                return ThirdNum;
+            }
+            throw new Exception("firstnumber,secondnumber and thirdnumber are same");
+        }
+
+
+    }
+}
