94afa1c5c8d89b71bcaf18f20bce4c37d78a3941
AtulSikarwal <atulsikarwal5.com>
2022-06-21 17:14:17 +0530

    The code for Generic Method

---
+++ Refactor_GenricMethod/Method.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAss
+{
+    internal class Method
+    {
+        public int intMax(int FirstNum,int SecondNum,int ThirdNum)
+        {
+            if (FirstNum.CompareTo(SecondNum) > 0 && FirstNum.CompareTo(ThirdNum) > 0)
+            {
+                return FirstNum;
+            }
+            if (SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) > 0)
+            {
+                return SecondNum;
+            }
+            if (ThirdNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(FirstNum) > 0)
+            {
+                return ThirdNum;
+            }
+            throw new Exception("firstnumber,secondnumber and thirdnumber are same");
+        }
+
+
+
+        public double floatMax(double FirstNum, double SecondNum, double ThirdNum)
+        {
+            if (FirstNum.CompareTo(SecondNum) > 0 && FirstNum.CompareTo(ThirdNum) > 0)
+            {
+                return FirstNum;
+            }
+            if (SecondNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(ThirdNum) > 0)
+            {
+                return SecondNum;
+            }
+            if (ThirdNum.CompareTo(FirstNum) > 0 && SecondNum.CompareTo(FirstNum) > 0)
+            {
+                return ThirdNum;
+            }
+            throw new Exception("firstnumber,secondnumber and thirdnumber are same");
+        }
+
+
+      public string FindMaxString(string firstString,string secondString,string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+              firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+
+               firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+                return firstString;
+            }
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+
+                 secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                return secondString;
+            }
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+
+                 thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+            return firstString;
+        }
+
+
+    }
+}
---
+++ Refactor_GenricMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericsAss
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Method method = new Method();
+
+            Console.WriteLine(method.intMax(50, 39, 22));
+            Console.WriteLine(method.floatMax(50.2, 39.2, 2.2));
+            Console.WriteLine(method.FindMaxString("Atul","Arjun","Sikarwal"));
+
+            GenricMethod result=new GenricMethod();
+               result.ToMax<int>(3, 4, 2);
+            result.ToMax<double>(3.3, 4.2, 2.1);
+            result.ToMax<string>("Atul", "Arjun", "Sikarwal");
+
+        }
+    }
+}
